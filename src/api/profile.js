5d12d70b36fa53756259df621cb999080160720b
E Prasetyo <67994910+E-Prasetyo@users.noreply.github.com>
2022-05-21 22:43:40 +0700

    update Login

--- src/api/profile.js
+++ src/api/profile.js
@@ -1,1 +1,15 @@
 // TODO: answer here
+import axios from 'axios';
+import { API_URL } from './config';
+  
+export const getProfile = async(idUser) => {
+    const data = await axios.get(
+        API_URL+`/profile/${idUser}`,
+        { withCredentials: true }
+        )
+      .then((response) => response.data);
+  
+      return data ? data : []
+}
+  
+  // // const 
